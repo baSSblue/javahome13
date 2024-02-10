a13b2abf413f5cbcbc0537f37f2ff55733ec9a73
Bella <bella.kallen2014@gmail.com>
2024-02-10 21:31:18 +0300

    webdriver doesn't work

--- src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
@@ -1,11 +1,32 @@
 package ru.netology.webselenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 
 public class AppOrderPositiveTest {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setupAll() { WebDriverManager.chromedriver().setup(); }
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach (){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        driver.quit();
+        driver = null;
+    }
 }
